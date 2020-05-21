90d0100b2fc3d2dfe0551935fb2e41d96effa571
MartinlLee <yanzhou.li@my.jcu.edu.au>
2020-05-21 20:38:15 +0800

    Add Vehicle class,and build Vehicle start and stopped position.

---
+++ Traffic simulator/src/Model/Vehicle.java
@@ -0,0 +1,9 @@
+package Model;
+
+import java.awt.*;
+import java.util.Random;
+
+public abstract class Vehicle {
+
+    private static final int STOPPED = 0;
+    private static final int START_POSITION = 0;
