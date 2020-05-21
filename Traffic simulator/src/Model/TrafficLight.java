0d6fda610251d45bc548a85b56ea9d5fa4a2c241
MartinlLee <yanzhou.li@my.jcu.edu.au>
2020-05-21 20:46:33 +0800

    Add TrafficLight class,and build int of TrafficLight(green and red light)

---
+++ Traffic simulator/src/Model/TrafficLight.java
@@ -0,0 +1,14 @@
+package Model;
+
+import java.awt.*;
+import java.util.Random;
+
+public class TrafficLight {
+    private static final double CHANGE = 0.4; // more often red
+    private static final String GREEN = "green";
+    private static final String RED = "red";
+    private String id;
+    private String state;
+    private int position;
+    private Road roadAttachedTo;
+
